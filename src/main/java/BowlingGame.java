a333f382bb20648800ee20446d408b289f8dd944
Marc Nazarian <marc.nazarian@gmail.com>
2018-02-01 23:42:29 +0100

    Extracted numbers into constants

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -2,15 +2,18 @@ import java.util.List;
 
 public class BowlingGame {
 
+    private static final int NUMBER_OF_FRAMES = 10;
+    private static final int TOTAL_NUMBER_OF_PINS = 10;
+
     public static int computeScore(List<Integer> rolls) {
         int gameScore = 0;
         int frameIndex = 0;
-        for (int frame = 0; frame < 10; frame++) {
+        for (int frame = 0; frame < NUMBER_OF_FRAMES; frame++) {
             if (isStrike(frameIndex, rolls)) {
-                gameScore += 10 + computeStrikeBonus(rolls, frameIndex);
+                gameScore += TOTAL_NUMBER_OF_PINS + computeStrikeBonus(rolls, frameIndex);
                 frameIndex++;
             } else if (isSpare(frameIndex, rolls)) {
-                gameScore += 10 + computeSpareBonus(rolls, frameIndex);
+                gameScore += TOTAL_NUMBER_OF_PINS + computeSpareBonus(rolls, frameIndex);
                 frameIndex += 2;
             } else {
                 gameScore += rolls.get(frameIndex) + rolls.get(frameIndex + 1);
@@ -21,7 +24,7 @@ public class BowlingGame {
     }
 
     private static boolean isSpare(int frameIndex, List<Integer> rolls) {
-        return frameIndex + 1 <= rolls.size() && rolls.get(frameIndex) + rolls.get(frameIndex + 1) == 10;
+        return rolls.get(frameIndex) + rolls.get(frameIndex + 1) == TOTAL_NUMBER_OF_PINS;
     }
 
     private static int computeSpareBonus(List<Integer> rolls, int frameIndex) {
@@ -29,7 +32,7 @@ public class BowlingGame {
     }
 
     private static boolean isStrike(int frameIndex, List<Integer> rolls) {
-        return rolls.get(frameIndex) == 10;
+        return rolls.get(frameIndex) == TOTAL_NUMBER_OF_PINS;
     }
 
     private static int computeStrikeBonus(List<Integer> rolls, int frameIndex) {
